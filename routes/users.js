595904094ccbf72dac3b49ce20ffe565ccb31969
Dante Tam <dante.tam1@gmail.com>
2018-01-04 10:13:20 -0800

    Website formatting changes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/login', function(req, res, next) {
+  res.send('Test endpoint GET /users/login');
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
